0780216511d56c1a519ff231db5f16099a8eb06f
Mitchell Franklin <mitchellfranklin@gmail.com>
2020-09-14 16:02:06 +1000

    1.5.1 Hotfix - repair for show header button due to changes in ID contexts

--- src/library/boomiapp/boomi.js
+++ src/library/boomiapp/boomi.js
@@ -1,19 +1,18 @@
-$(document).ready(function () {
-  /*Start of Sheet*/
 
-  //Only Actions occur once the screen has been fully loaded
-  var boomiatomLoaded = setInterval(function () {
-    if (document.body.contains(document.getElementById("mastfoot"))) {
-      $("#gwt-uid-84").append(
-        '<li id="showHeaderbtn" class="qm-c-servicenav__nav-item"><a class="gwt-Anchor qm-c-servicenav__nav-link qm-a--alternate"><span id="showHeaderspan" class="">Show Header</span></a></li>'
-      );
 
-      clearInterval(boomiatomLoaded); //clear the interval now that data has been loaded
-    }
 
-    //end of execution once
-  }, 1000);
-  //////////
+  let boomi_title = document.title;
+  let boomiatomLoaded = setInterval(()=>{
+  
+      if(boomi_title != document.title){
+          clearInterval(boomiatomLoaded);
+          var headerAdd = document.getElementsByClassName('qm-c-servicenav__navbar')[0].id
+          $('#'+headerAdd).append(
+            '<li id="showHeaderbtn" class="qm-c-servicenav__nav-item"><a class="gwt-Anchor qm-c-servicenav__nav-link qm-a--alternate"><span id="showHeaderspan" class="">Show Header</span></a></li>'
+          );
+      }
+  
+  },250)
 
-  //END
-});
+
+  
