5280e7bb08a872407dbe7219b98bf5e87cfed902
Mitch Franklin <mitchellfranklin@gmail.com>
2021-05-24 11:08:33 +1000

    1.5.5.1 Hotfix
    Hotfix on Canvas Removal, fixed issue with only the first Canvas background being removed

--- src/library/boomiapp/canvas.js
+++ src/library/boomiapp/canvas.js
@@ -1,8 +1,11 @@
-
 const add_canvase_listener = (canvas) => {
 
-    if (BoomiPlatform.canvas_grid == 'off'){
+    if (BoomiPlatform.canvas_grid == 'off') {
         let xpanel = document.getElementsByClassName("canvas_grid");
-        xpanel[0].classList.add('canvasNew')
+
+        for (var index = 0; index < xpanel.length; index++) {
+            xpanel[index].classList.add('canvasNew')
+        }
+
     }
-    }
+}
