c781f4d214a4fdd69ab7be72893e3b116fce0713
Mitch Franklin <mitchellfranklin@gmail.com>
2021-10-22 10:37:28 +1000

    1.6.0.2 Hitfix
    Resolution of missing getUrlParameter function require to enable copy component ID process

--- src/library/boomiapp/listenerGlobal.js
+++ src/library/boomiapp/listenerGlobal.js
@@ -76,3 +76,19 @@ window.addEventListener('message', (e) => {
         }
     }
 }, false);
+
+
+let getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = $(location).attr('href'),
+        sURLVariables = sPageURL.split(';'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
