32a9ec5c36d6ce25c116571591d43784ff9957ad
Mitch Franklin <mitchellfranklin@gmail.com>
2022-01-14 15:36:59 +1000

    Update Notification Coded, tested and ready

--- src/library/boomiapp/boomi.js
+++ src/library/boomiapp/boomi.js
@@ -3,10 +3,12 @@
   
       if(boomi_title != document.title){
           clearInterval(boomiatomLoaded);
+
           var headerAdd = document.getElementsByClassName('qm-c-servicenav__navbar')[0].id
           $('#'+headerAdd).append(
             '<li id="showHeaderbtn" class="qm-c-servicenav__nav-item"><a class="gwt-Anchor qm-c-servicenav__nav-link qm-a--alternate"><span id="showHeaderspan" class="">Show Header</span></a></li>'
   );
+  updatenotificationCheck();
       }
   
   },250)
