315426f01e9cc40210762d51929cc163de6b82cc
Boomi Developer <boomian@icloud.com>
2022-08-18 19:27:00 +0000

    Fixed issue where homepage header wouldn't show... Added a new onNavigationChange function that is called when the page is changed which then waits for the homepage header to be added to the DOM then shows it if on the homepage.

--- src/library/boomiapp/boomi.js
+++ src/library/boomiapp/boomi.js
@@ -2,16 +2,47 @@
   let boomiatomLoaded = setInterval(()=>{
   
       if(boomi_title != document.title){
-          clearInterval(boomiatomLoaded);
 
-          var headerAdd = document.getElementsByClassName('qm-c-servicenav__navbar')[0].id
-          $('#'+headerAdd).append(
-            '<li id="showHeaderbtn" class="qm-c-servicenav__nav-item"><a class="gwt-Anchor qm-c-servicenav__nav-link qm-a--alternate"><span id="showHeaderspan" class="">Show Header</span></a></li>'
+          var headerAdd = document.getElementsByClassName('qm-c-servicenav__navbar')[0];
+
+          // add the header if we can find it, if not keep polling
+          if(headerAdd){
+            $('#'+headerAdd.id).append(
+              '<li id="showHeaderbtn" class="qm-c-servicenav__nav-item"><a class="gwt-Anchor qm-c-servicenav__nav-link qm-a--alternate"><span id="showHeaderspan" class="">Show Header</span></a></li>'
+            );
+            // clear the setInterval timer to poll for injecting the Show Header Button
+            clearInterval(boomiatomLoaded);
+          }
   );
   updatenotificationCheck();
       }
   
   },250)
+    
+  function onNavigationChange(){
+    var urlPath = getUrlpath();
 
+    // Show Homepage Header
+    if(urlPath.includes("home")){
+      headerVisible = true;
+      let waitToShowHomepageHeader = setInterval(function(){
+        var x = document.getElementsByClassName("qm-c-masthead");
+        if(x){
+          x[0].classList.add("headerShow");
+          $("#showHeaderspan").text("Hide Header");
+          headerVisible = true;
+          clearInterval(waitToShowHomepageHeader);
+        }
+    }, 250);
+    }else{
+      clearInterval(waitToShowHomepageHeader);
+    }
+    
+  }
 
-  
+  // run on inital load
+  onNavigationChange()
+
+  // run on window change states
+  window.addEventListener('popstate', onNavigationChange);
+  window.addEventListener('onhashchange', onNavigationChange);
