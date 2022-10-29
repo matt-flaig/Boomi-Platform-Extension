1f8a263f3bfa3d542fd64a612aef87e9d63516db
Mitch Franklin <mitchellfranklin@gmail.com>
2022-10-29 10:37:33 +1000

    Adjustments to Refresh Process enhancement and Re-alignment of interval name from testing

--- src/library/boomiapp/customrefresh.js
+++ src/library/boomiapp/customrefresh.js
@@ -7,7 +7,12 @@ const refreshInterval_listener = (refreshInterval) => {
         BoomiPlatform.refresh_interval = 15;
     }
 
-    $('.reporting_right_side').prepend($('<li id="refresh_reporting"> <button  type="button" class="qm-button--primary-action" alt="off">Start Refresh</button> </li>'));
+
+
+
+
+
+    $('.reporting_right_side').prepend($('<li id="refresh_reporting"> <button  type="button" class="refresh_primary_action" alt="off">Refresh Every '+ BoomiPlatform.refresh_interval +'s</button> </li>'));
 
     $('#refresh_reporting').click(function (event) {
         event.preventDefault();
@@ -15,14 +20,18 @@ const refreshInterval_listener = (refreshInterval) => {
         var e = $(this).find('button');
         if (e.attr('alt') == 'off') {
             start_refresh();
-            e.text("Stop Refresh");
+            e.text('Refreshing Every '+ BoomiPlatform.refresh_interval +'s');
             e.attr('alt', 'on');
             e.blur()
+            e.addClass('refresh_doing_action')
+            e.removeClass('refresh_primary_action')
         } else {
             stop_refresh();
-            e.text("Start Refresh");
+            e.text('Refresh Every '+ BoomiPlatform.refresh_interval +'s');
             e.attr('alt', 'off');
             e.blur()
+            e.removeClass('refresh_doing_action')
+            e.addClass('refresh_primary_action')
         }
 
     })
@@ -57,9 +66,9 @@ const refreshInterval_listener = (refreshInterval) => {
         var vali = !processreportingCheck.includes("reporting") && child != null;
         if (vali == true) {
 
-            var child = document.getElementById("refresh_reporting") //.remove;
+            var child = document.getElementById("refresh_reporting")
 
-            // Remove the child element from the document
+
             child.parentNode.removeChild(child);
 
 
