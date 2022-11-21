b27159602c063b2e8a60679e3bd27affb2e3dc65
Mitch Franklin <mitchellfranklin@gmail.com>
2022-11-21 12:03:44 +1000

    Code and File Tidy Up

--- src/library/boomiapp/contentScript.js
+++ src/library/boomiapp/contentScript.js
@@ -10,8 +10,6 @@ loadScript('./library/boomiapp/descriptionMarkdown.js');
 loadScript('./library/boomiapp/groups.js');
 loadScript('./library/boomiapp/shapes.js');
 loadScript('./library/boomiapp/fullscreen.js');
-loadScript('./library/boomiapp/jsonView.js');
-loadScript('./library/boomiapp/sqlView.js');
 loadScript('./library/boomiapp/imageCapture.js');
 loadScript('./library/boomiapp/tableWrap.js');
 loadScript('./library/boomiapp/iconSets.js');
