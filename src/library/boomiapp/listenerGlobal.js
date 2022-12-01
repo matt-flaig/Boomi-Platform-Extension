fa60b6bd3d131f142e5d55ffc43447caa008d5d0
Mitch Franklin <mitchellfranklin@gmail.com>
2022-12-01 21:46:28 +1000

    1.7.3.6 head build

--- src/library/boomiapp/listenerGlobal.js
+++ src/library/boomiapp/listenerGlobal.js
@@ -116,6 +116,8 @@ const BoomiPlatform_Init = () => {
         listenerClass('.note-content', add_notecontent_listener);
         listenerClass('.auto_refresh_li', refreshInterval_listener);
         listenerClass('.gwt-DetailAreaInner', add_connector_list);
+        // listenerClass('.packaged_components_wizard',reminder_save);
+        listenerClass('.popupContent',reminder_schedule);
         listenerClass('.qm-c-inlinemenu--servicenav-menu-constrain',add_menu_dropdown)
 
     }, 1000)
--- src/library/boomiapp/updateNotification.js
+++ src/library/boomiapp/updateNotification.js
@@ -27,28 +27,10 @@ function updateNotificationfun() {
 let htmlUpdateContents = `
 <ul>
 <li>
-<p><strong>Feature:</strong> Brand new code editor attached to Message, Notify and Command Shape, click Edit message to open up a dedicated code window with choice of code display.</p>
-</li>
-<li>
-<p><strong>Feature:</strong> Tab names remove the account name to enable better display of tabs function i.e. Build, Reporting etc. Toggle Option Available.</p>
-</li>
-<li>
-<p><strong>Feature:</strong> New Icon against Drop Down Menu items i.e. Manage to allow you to click and open in new tab instead of the same window.</p>
-</li>
-<li>
-<p><strong>Feature:</strong> Default SQL Editor in Data Base Operation code area is now resizable for use with larger screens.</p>
-</li>
-<li>
-<p><strong>Change:</strong> Connector Operation Screen for HTTP has increased sizing on Request details for better use.</p>
-</li>
-<li>
-<p><strong>Change:</strong> Process Description Edit Box width increased to enable full view of description when editing.</p>
-</li>
-<li>
-<p><strong>Bugfix:</strong> New logo not appearing in options page properly.</p>
-</li>
-<li>
 <p><strong>Bugfix:</strong> Incorrectly applied flex css as this was applied to all windows - only applys to editor screens.</p>
+</li>
+<li>
+<p><strong>Bugfix:</strong> Resolution of Dot Background not appearing in light mode (canvas).</p>
 </li>
 </ul>
 `
