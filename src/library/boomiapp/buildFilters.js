972564cbc7f9ae30894f6e42811698ffa475238d
Mitchell Franklin <mitchellfranklin@gmail.com>
2023-07-13 23:04:40 +1000

    Fix of Filter Issue Bug and Notification Update

--- src/library/boomiapp/buildFilters.js
+++ src/library/boomiapp/buildFilters.js
@@ -13,12 +13,23 @@ document.arrive('.filter_popup', function (filteredScreen) {
     chrome.storage.sync.get(['Filter_crossref'], function (e) {
         CrossFilter = e.Filter_crossref
     })
+    chrome.storage.sync.get(['Filter_api_service'], function (e) {
+        APIServFilter = e.Filter_api_service
+    })
 
     chrome.storage.sync.get(['apply_process_filters'], function (e) {
         if (e.apply_process_filters === 'on') {
-            document.getElementById('gwt-uid-389').checked = processFilter
-            document.getElementById('gwt-uid-391').checked = processPropFilter
-            document.getElementById('gwt-uid-381').checked = CrossFilter
+ 
+            var matchingxref = document.evaluate("//label[contains(text(),'Cross Reference Table')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            var matchingprocess = document.evaluate("//label[contains(text(),'Process')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            var matchingproprop = document.evaluate("//label[contains(text(),'Process Property')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+            var matchingapiserv= document.evaluate("//label[contains(text(),'API Service')]", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+
+
+            document.getElementById(matchingprocess.htmlFor).checked = processFilter
+            document.getElementById(matchingproprop.htmlFor).checked = processPropFilter
+            document.getElementById(matchingxref.htmlFor).checked = CrossFilter
+            document.getElementById(matchingapiserv.htmlFor).checked = APIServFilter
         }
     })
 })
