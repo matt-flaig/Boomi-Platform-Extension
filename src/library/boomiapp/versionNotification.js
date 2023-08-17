13f3c29d38d6a71b3e268e2174383d5f52bdf3c7
Matthew Flaig <hi@mattflaig.com>
2023-08-16 19:54:17 -0700

    Remove space

--- src/library/boomiapp/versionNotification.js
+++ src/library/boomiapp/versionNotification.js
@@ -8,7 +8,7 @@ for (var index = 0; index < notipanel.length; index++) {
     if (notinav) {
     notipanel[index].children[1].children[1].insertAdjacentHTML(
         'afterend',
-        '<span id="close" onclick="this.parentNode.parentNode.querySelector(\'.component_header\').style.background=\'#f5e4c2\';this.parentNode.remove(); return false;" style="cursor:pointer;position: absolute; right: 0; top: 0; margin: 2px">x</span>'
+        '<span id="close" onclick="this.parentNode.parentNode.querySelector(\'.component_header\').style.background=\'#f5e4c2\';this.parentNode.remove();return false;" style="cursor:pointer;position: absolute; right: 0; top: 0; margin: 2px">x</span>'
     );
     }
 }
