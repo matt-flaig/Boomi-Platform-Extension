b1388f8011a380279f7ba2cfc099b0ca9a12dd9b
Matthew Flaig <info@buzztouch.com>
2023-09-23 00:48:04 -0700

    Adjusted Table Wrap configuration options

--- src/library/boomiapp/tableWrap.js
+++ src/library/boomiapp/tableWrap.js
@@ -1,47 +1,51 @@
 const add_table_listener = (table) => {
-    if (BoomiPlatform.tablewrap_selector !== 'off') {
-    let head = table.querySelector('thead');
-    let wrapped = false;
-    let wrapper = null;
-    if (!head) return false;
+    // the "on" clause below is a legacy patch to keep the toggle showing
+    // if they haven't selected one of the newer options (toggle, always, never)
+    if (BoomiPlatform.tablewrap_selector == 'toggle' || BoomiPlatform.tablewrap_selector == "on"){ // Toggle Option On Header Hover
+        let head = table.querySelector('thead');
+        let wrapped = false;
+        let wrapper = null;
+        if (!head) return false;
 
-    if (table.closest('.wrapped_text_column_style')) {
-        wrapped = true;
-        wrapper = table.closest('.wrapped_text_column_style');
-        wrapper.classList.add('bph-table-wrapped');
-    }
-
-    let over = false;
-    head.addEventListener('mouseover', event => {
-        over = true;
-        if (!head.querySelector('.bph-thead-menu')) {
-            let menuHTML = `<div class="bph-thead-menu">
-
-                <a class='toggle_word_wrap'>Toggle Table Line Wrap</a>
-
-            </div>`;
-            head.insertAdjacentHTML('beforeend', menuHTML);
-            head.querySelector('.bph-thead-menu').style.display = 'block';
-
-            head.querySelector('.bph-thead-menu .toggle_word_wrap').addEventListener('click', event => {
-
-                if (wrapper) wrapper.classList.toggle('wrapped_text_column_style');
-                table.classList.toggle('bph-no-wrap');
-            });
-
-        } else {
-            head.querySelector('.bph-thead-menu').style.display = 'block'
+        if (table.closest('.wrapped_text_column_style')) {
+            wrapped = true;
+            wrapper = table.closest('.wrapped_text_column_style');
+            wrapper.classList.add('bph-table-wrapped');
         }
-    });
 
-    head.addEventListener('mouseout', event => {
-        over = false;
-        setTimeout(() => {
-            if (head.querySelector('.bph-thead-menu') && !over) {
-                head.querySelector('.bph-thead-menu').style.display = 'none';
+        let over = false;
+        head.addEventListener('mouseover', event => {
+            over = true;
+            if (!head.querySelector('.bph-thead-menu')) {
+                let menuHTML = `<div class="bph-thead-menu">
+
+                    <a class='toggle_word_wrap'>Toggle Table Line Wrap</a>
+
+                </div>`;
+                head.insertAdjacentHTML('beforeend', menuHTML);
+                head.querySelector('.bph-thead-menu').style.display = 'block';
+
+                head.querySelector('.bph-thead-menu .toggle_word_wrap').addEventListener('click', event => {
+
+                    if (wrapper) wrapper.classList.toggle('wrapped_text_column_style');
+                    table.classList.toggle('bph-wrap');
+                    //console.log('Table Wrap State = ' + !table.classList.contains('bph-wrap'));
+                });
+
+            } else {
+                head.querySelector('.bph-thead-menu').style.display = 'block'
             }
-        }, 100)
-    });
-} 
+        });
 
+        head.addEventListener('mouseout', event => {
+            over = false;
+            setTimeout(() => {
+                if (head.querySelector('.bph-thead-menu') && !over) {
+                    head.querySelector('.bph-thead-menu').style.display = 'none';
+                }
+            }, 100)
+        });
+    }else if(BoomiPlatform.tablewrap_selector == 'always'){ // Never Wrap Option
+        table.classList.add('bph-wrap');
+    }
 }
