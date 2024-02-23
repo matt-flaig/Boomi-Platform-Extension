70deb90c16ee7bccb980036784de48f91b69b520
matt-flaig <hi@mattflaig.com>
2024-02-22 22:37:44 -0800

    Fix update notification indentation
    Changed updatenotificationCheck to camelCase.
    
    Changed updateNotificationfun function to updateNotificationAlert

--- src/library/boomiapp/boomi.js
+++ src/library/boomiapp/boomi.js
@@ -31,7 +31,7 @@ let boomiPageLoaded = setInterval(()=>{
           });
         }
       onNavigationChange();
-      updatenotificationCheck();
+      updateNotificationCheck();
     }
 
 }, 250);
