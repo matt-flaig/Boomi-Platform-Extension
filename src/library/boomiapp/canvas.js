73e33c00d43f439aac07429b9e6c3bc6407e250f
matt-flaig <hi@mattflaig.com>
2024-02-22 22:41:28 -0800

    Renamed canvasNew to canvas_grid

--- src/library/boomiapp/canvas.js
+++ src/library/boomiapp/canvas.js
@@ -1,10 +1,10 @@
 const add_canvas_listener = (canvas) => {
 
     if (BoomiPlatform.canvas_grid == 'off') {
-        let xpanel = document.getElementsByClassName("canvas_grid");
+        let xpanel = document.getElementsByClassName("canvas_grid_process_editor");
 
         for (var index = 0; index < xpanel.length; index++) {
-            xpanel[index].classList.add('canvasNew')
+            xpanel[index].classList.add('hide_canvas_grid')
         }
 
     }
